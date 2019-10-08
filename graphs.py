866905ad44c620b2b87744dbe7347ee5dff14122
andrew <andrew.j.bergman@gmail.com>
2019-10-08 11:24:00 -0400

    added docstrings

--- graphs.py
+++ graphs.py
@@ -1,14 +1,20 @@
+# Imports 
+
 import seaborn           as sns
 import numpy             as np
 import matplotlib.pyplot as plt
+from sklearn.metrics     import roc_auc_score
 
 #####
+
+"""Setting the basic appearance for the graphs"""
 
 sns.set(style = "white", palette = "deep")
 
 #####
 
 def histograms(columns, df, titles, labels, ylabel, ticks, row, col):
+    """Creates histograms for continuous data"""
     count = 0
     fig   = plt.figure(figsize = (14,7), facecolor = "white")
     for c, column in enumerate(columns):
@@ -26,6 +32,7 @@ def histograms(columns, df, titles, labels, ylabel, ticks, row, col):
 #####
 
 def boxplots(columns, df, titles, labels, ticks, row, col):
+    """Creates boxplots for continuous data"""
     count = 0
     fig   = plt.figure(figsize = (14,7, facecolor = "white"))
     for c, column in enumerate(columns):
@@ -41,7 +48,8 @@ def boxplots(columns, df, titles, labels, ticks, row, col):
 
 #####
 
-def scatterplots(columns, y, df, titles, labels, ylabel, ticks, row, col, mark, ci = None):
+def regressionplots(columns, y, df, titles, labels, ylabel, ticks, row, col, mark, ci = None):
+    """Creates regression plots for continuous data."""
     count = 0
     fig   = plt.figure(figsize = (14,7), facecolor = "white")
     for c, column in enumerate(columns):
@@ -59,6 +67,7 @@ def scatterplots(columns, y, df, titles, labels, ylabel, ticks, row, col, mark, 
 #####
 
 def countplots(columns, df, titles, labels, ylabel, row, col):
+    """Creates a count plot for categorical data.  This type of plot explicityly counts the categories in a dataframe column."""
     fig   = plt.figure(figsize = (20,30), facecolor = "white")
     count = 0
     for c, column in enumerate(columns):
@@ -77,6 +86,7 @@ def countplots(columns, df, titles, labels, ylabel, row, col):
 #####
 
 def barplots(df, x, y, labels, ylabel, titles, row, col):
+    """Generates barplots for categorical data"""
     fig   = plt.figure(figsize = (20,30), facecolor = "white")
     count = 0
     for c, column in enumerate(columns):
@@ -94,20 +104,22 @@ def barplots(df, x, y, labels, ylabel, titles, row, col):
 
 #####
 
-def heatmap(df, title, vmin, vmax, cmap = "RdBu"):
+def heatmap(df, columns, title, vmin, vmax):
+    """Creates a heatmap for dataframe columns.  The visualization includes a a correlation bar for, annotations, and is not mirrored."""
     plt.figure(figsize = (16,8), facecolor = "white")
     plt.title(f"title", size = 18)
     corr = df[columns].corr()
     mask = np.zeros_like(corr)                                                                                
     mask[np.triu_indices_from(mask)] = True
     with sns.axes_style("white"):
-        sns.heatmap(corr, cmap = cmap, mask = mask, vmin = vmin, vmax = vmax, annot = True)
+        sns.heatmap(corr, cmap = "RdBu", mask = mask, vmin = vmin, vmax = vmax, annot = True)
     plt.xticks(size = 14)
     plt.yticks(size = 14);
 
 #####  The ROC curve code was modified from code written by Matt Brems during our lesson on classification metrics.
 
 def roc_curve(model_prob, X_test, y_test, y_pred, title, roc_color = "darkorange", baseline_color = ):
+    """Creates a ROC-AUC curve with the AUROC score included in the title."""
         model_prob    = [i[0] for i in model_prob.predict_proba(X_test)]
         model_pred_df = pd.DataFrame({"true_values": y_test, "pred_probs": model_prob})
     thresholds = np.linspace(0, 1, 500) 
@@ -123,8 +135,9 @@ def roc_curve(model_prob, X_test, y_test, y_pred, title, roc_color = "darkorange
     fpr_values = [false_positive_rate(model_pred_df, "true_values", "pred_probs", prob) for prob in thresholds]
     plt.figure(figsize   = (13,7), facecolor = "white")
     plt.plot(fpr_values, tpr_values, color = roc_color, label = "ROC Curve")
-    plt.plot(np.linspace(0, 1, 500), np.linspace(0, 1, 500), color = baseline_color, label = "Baseline"),
-    plt.title(title, fontsize = 18)
+    plt.plot(np.linspace(0, 1, 500), np.linspace(0, 1, 500), color = baseline_color, label = "Baseline")
+    rocauc_score = round(roc_auc_score(y_test, y_preds), 5)
+    plt.title(f"{title} With A Score of {}", fontsize = 18)
     plt.ylabel("Sensitivity", size = 16)
     plt.xlabel("1 - Specificity", size = 16)
     plt.xticks(size = 14)
--- metrics.py
+++ metrics.py
@@ -3,17 +3,24 @@ from sklearn.metrics import confusion_matrix, r2_score
 
 ###
 
+# Regression Metrics
+
 def r2_adj(X, y_true, y_predicted):
+    """Calculates an adjusted R^2 score which is scaled to the number of features in the model: the R^2 score is often inflated by a large number of features."""
     r2     = r2_score(y_true, y_predicted)
     r2_adj = 1 - ((1 - r2) * (len(y) - 1)) / (len(y) - len(X.columns)) - 1
     return r2_adj
 
+# Classification Metrics
+
 def gen_confusion_matrix(y, y_preds, columns, index):
+    """An improvement on the standard SKLearn confusion matrix which returns a Pandasd dataframe and includes labels."""
     cm     = confusion_matrix(y, y_preds)
     matrix = pd.DataFrame(cm, columns = columns, index   = columns)
     return matrix
 
 def specificity(y, y_pred):
+    """Evaluates the number of true negatives out of all negatives.  This score is not available in SKLearn."""
     cm          = confusion_matrix(y, y_pred)  
     specificity = cm[0,0] / (cm[0,0] + cm[0,1])
     return specificity
